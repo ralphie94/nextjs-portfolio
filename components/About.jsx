afe55d92e3a51df0d0f79530600171619b5e8104
Ralphie Celedon <ralphie94@pacbell.net>
2023-10-12 15:30:43 -0700

    built out contact section

--- components/About.jsx
+++ components/About.jsx
@@ -19,8 +19,8 @@ const About = () => {
                 </p>
                 <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p>
             </div>
-            <div className='w-[500px] h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <img className='rounded-xl' src="/assets/laptop.jpg"></img>
+            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+                <img className='rounded-xl' src="/assets/projects/ralphie.png"></img>
             </div>
         </div>
     </div>
