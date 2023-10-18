8a554d13edb2ac8564b4719f9be5fb0be0d7e25e
Ralphie Celedon <ralphie94@pacbell.net>
2023-10-18 13:33:17 -0700

    adding content

--- components/About.jsx
+++ components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                 <Link href='/#projects'><p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p></Link>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                <Image className='rounded-xl' src="/assets/projects/ralphie.png" width='500' height='50' alt='/' />
+                <Image className='rounded-xl' src="/assets/projects/ralphie.jpg" width='500' height='50' alt='/' />
             </div>
         </div>
     </div>
